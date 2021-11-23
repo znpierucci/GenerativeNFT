1dfe2a843936d5a8b60ac0f14d69180dcaa19fb6
Zachary Pierucci <znpierucci@gmail.com>
2021-11-23 01:57:25 -0600

    almost done

---
+++ FeatureModels/config.js
@@ -0,0 +1,73 @@
+const fs = require("fs");
+const dir = __dirname;
+
+const width = 1000;
+const height = 1000;
+
+//rarity file extensions
+const rarity = [
+    {key: "", val: "common"},
+    {key: "_uncommon", val: "uncommon"},
+    {key: "_rare", val: "rare"},
+    {key: "_super", val: "super rare"},
+    {key: "_legendary", val: "legendary"},
+];
+
+//checks for rarity filename ending
+const getRarity = _str => {
+    let itemRarity;
+  
+    rarity.forEach((r) => {
+      if (_str.includes(r.key)) {
+        itemRarity = r.val;
+      }
+    });
+  
+    return itemRarity;
+  };
+
+//remove the rarity and file extension of the image - file must be png/jpg (3 chars long)
+const cleanName = _str => {
+    let name = _str.slice(0, -4);
+    rarity.forEach((r) => {
+      name = name.replace(r.key, "");
+    });
+    return name;
+  };
+
+//
+const getElements = (path) => {
+    return fs
+        .readdirSync(path)
+        .filter((item) => !/(^|\/)\.[^\/\.]/g.test(item))
+        .map((i, index) => {
+            return {
+                id: index + 1,
+                name: cleanName(i),
+                fileName: i,
+                rarity: getRarity(i),
+            };
+        });
+};
+
+//layers of the NFT in order
+const layers = [
+    {
+        id: 1,
+        name: "background",
+        location: `${dir}/Backgrounds/`,
+        elements: getElements(`${dir}/Backgrounds/`),
+        position: {x: 0, y: 0},
+        size: {width: width, height: height},
+    },
+    {
+        id: 2,
+        name: "base model",
+        location: `${dir}/BaseModels/`,
+        elements: getElements(`${dir}/BaseModels/`),
+        position: {x: 0, y: 0},
+        size: {width: width, height: height},
+    },
+];
+
+module.exports = {layers, width, height};
--- index.js
+++ index.js
@@ -1,18 +1,73 @@
 const fs = require("fs");
+const myArgs = process.argv.slice(2);
 const {createCanvas, loadImage} = require("canvas");
-const canvas = createCanvas(1000, 1000);
+const {layers, width, height} = require("./FeatureModels/config.js")
+const console = require("console");
+const canvas = createCanvas(width, height);
 const context = canvas.getContext("2d");
 
-const drawImage = async () => {
-    const image = await loadImage("./FeatureModels/BaseModels/beanos.png");
-    context.drawImage(image, 0, 0, 1000, 1000);
-    console.log("completed draw.");
-    saveImage(canvas);
+//first argument after <node index.js> will determine number of NFTs to be generated
+const nftCount = myArgs.length > 0 ? (myArgs[0]) : 1;
+
+var metadata = [];
+var attributes = [];
+var hash = [];
+var decodedHash =[];
+
+//add metadata to NFT
+const addMetadata = (_nftCount) => {
+    let dateTime = Date.now();
+    let tempMetadata = {
+        hash: hash.join(""),
+        decodedHash: decodedHash,
+        id: _nftCount,
+        date: dateTime,
+        attributes: attributes,
+    };
+    metadata.push(tempMetadata);
+    attributes = [];
+    hash = [];
+    decodedHash = [];
 };
 
-const saveImage = (_canvas) => {
-    fs.writeFileSync("./ResultImages/test.png", _canvas.toBuffer("image/png"));
-    console.log("completed save.");
+const addAttributes = (_element, _layer) => {
+    let tempAttributes = {
+        id: _element.id,
+        layer: _layer.name,
+        name: _element.name,
+        rarity: _element.rarity,
+    };
+    attributes.push(tempAttributes);
+    hash.push(_layer.id);
+    hash.push(_element.id);
+    decodedHash.push({[_layer.id]: _element.id});
 };
 
-drawImage();
+//draw each layer
+const drawLayer = async (_layer, _nftCount) => {
+    let element = _layer.elements[Math.floor(Math.random() * _layer.elements.length)];
+    addAttributes(element, _layer);
+    const image = await loadImage(`${_layer.location}${element.fileName}`);
+    context.drawImage(image, _layer.position.x, _layer.position.y, _layer.size.width, _layer.size.height);
+    saveImage(canvas, _nftCount);
+};
+
+//save each image
+const saveImage = (_canvas, _nftCount) => {
+    fs.writeFileSync(`./output/test#${_nftCount}.png`, _canvas.toBuffer("image/png"));
+};
+
+//loop for each layer of each nft and draw
+for (let i = 1; i <= nftCount; i++) {
+    layers.forEach(layer => {
+        drawLayer(layer, i);
+    })
+    addMetadata(i);
+    console.log("creating nft " + i);
+}
+
+//error handling metadata file write
+fs.readFile("./output/metadata.json", (err, data) => {
+    if (err) throw err;
+    fs.writeFileSync("./output/metadata.json", JSON.stringify(metadata));
+});
