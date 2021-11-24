5b1a874dcd19d447b1fff9c10bab463c0b86f1e2
Zachary Pierucci <znpierucci@gmail.com>
2021-11-23 21:40:35 -0600

    rarity

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const myArgs = process.argv.slice(2);
 const {createCanvas, loadImage} = require("canvas");
 const {layers, width, height} = require("./feature_models/config.js")
 const console = require("console");
+const { SSL_OP_LEGACY_SERVER_CONNECT } = require("constants");
 const canvas = createCanvas(width, height);
 const context = canvas.getContext("2d");
 
@@ -13,6 +14,8 @@ var metadata = [];
 var attributes = [];
 var hash = [];
 var decodedHash =[];
+var dnaList = [];
+var rarity = "";
 
 //add metadata to NFT
 const addMetadata = (_nftCount) => {
@@ -43,8 +46,31 @@ const addAttributes = (_element, _layer) => {
     decodedHash.push({[_layer.id]: _element.id});
 };
 
+const chooseRarity = () => {
+    rarityNum = Math.random();
+    switch (rarityNum) {
+        case rarityNum < 0.005:
+            rarity = "legendary";
+            break;
+        case rarityNum < 0.01:
+            rarity = "super rare";
+            break;
+        case rarityNum < 0.1:
+            rarity = "rare";
+            break;
+        case rarityNum < 0.4:
+            rarity = "uncommon";
+            break;
+        default:
+            rarity = "common";
+    }
+}
+
 //draw each layer
 const drawLayer = async (_layer, _nftCount) => {
+    //TODO: insert random function with weight
+    chooseRarity();
+    //choose from rarity
     let element = _layer.elements[Math.floor(Math.random() * _layer.elements.length)];
     addAttributes(element, _layer);
     const image = await loadImage(`${_layer.location}${element.fileName}`);
@@ -57,17 +83,37 @@ const saveImage = (_canvas, _nftCount) => {
     fs.writeFileSync(`./output/test#${_nftCount}.png`, _canvas.toBuffer("image/png"));
 };
 
-//loop for each layer of each nft and draw
-for (let i = 1; i <= nftCount; i++) {
-    layers.forEach(layer => {
-        drawLayer(layer, i);
-    })
-    addMetadata(i);
-    console.log("creating nft " + i);
-}
+//check for unique DNA
+const isDNAUnique = (_dnaList = [], _dna = []) => {
+    let foundDNA = _dnaList.find((i) => i.join("") === _dna.join(""));
+    return foundDNA == undefined ? true : false;
+};
 
-//error handling metadata file write
-fs.readFile("./output/metadata.json", (err, data) => {
-    if (err) throw err;
+//create each NFT's DNA
+const createDNA = (_length) => {
+    let random = Math.floor(Number(`1e${_length}`) + Math.random() * Number(`9e${_length}`));
+    return random;
+};
+
+//write our metadata
+const writeMetaData = () => {
+    //TODO: format it better
     fs.writeFileSync("./output/metadata.json", JSON.stringify(metadata));
-});
+};
+
+//loop for each layer of each nft and draw
+const startBatch = () => {
+    let currentNFT = 1;
+    while (currentNFT <= nftCount) {
+        layers.forEach(layer => {
+            drawLayer(layer, currentNFT);
+        })
+        addMetadata(currentNFT);
+        console.log("creating nft " + currentNFT);
+        currentNFT++;
+    }
+};
+
+//start script!
+startBatch();
+writeMetaData();
